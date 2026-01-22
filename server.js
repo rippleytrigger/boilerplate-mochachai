0b07dd833175b0a187c74bf38f9bf55511461861
Márques Niels López Medina <marques@MacBook-Air-de-Marques.local>
2026-01-22 22:08:20 +0100

    Test 4

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/_api/get-tests', cors(), function (req, res, next) {
   });
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', function () {
   console.log("Listening on port " + port);
   console.log('Running Tests...');
