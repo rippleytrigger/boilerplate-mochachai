22634dba9043b80703ced44e257765b6aad429b6
Márques Niels López Medina <marques@MacBook-Air-de-Marques.local>
2026-01-22 21:16:06 +0100

    Unit Test 1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,8 +5,8 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isNull(null, 'Is Null');
+      assert.isNotNull(true, 'Is not Null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
