f568722c60bee575a9bdea47d54c7791384f121f
Márques Niels López Medina <marques@MacBook-Air-de-Marques.local>
2026-01-25 16:27:05 +0100

    Test 25 done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -113,9 +113,21 @@ suite('Functional Tests with Zombie.js', function () {
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      const browser = new Browser();
 
-      done();
+        browser.visit('https://boilerplate-mochachai--rippleytrigger.replit.app', function () {
+
+        browser.fill('surname', 'Vespucci');
+        browser.pressButton('submit', function () {
+
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+
+          done();
+        });
+      });
     });
   });
 });
